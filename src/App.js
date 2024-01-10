922dbac1169504d7d9057339ffed778212c96a5a
OJane1 <o.s.petrashova@gmail.com>
2024-01-10 10:17:38 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import { data } from "./data";
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+const [course, setCourse] = useState(0);
+const { id, recipe, image, description } = data[course];
+const [showMore, setShowMore] = useState(false);
+
+const previousCourse = () => {
+  setCourse((course => {
+    course--;
+    if (course < 0) {
+      return data.length-1;
+    }
+    return course;
+  }))
+}
+
+const nextCourse = () => {
+  setCourse((course => {
+    course++;
+    if (course > data.length-1) {
+      course = 0;
+    }
+    return course;
+  }))
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+   <div className="container">
+      <h1>Top {data.length} Christmas recipies</h1>
     </div>
+<div className="container">
+      <img src={image} width="500px" alt="dish"/>
+    </div>
+    <div className="container" key={id}>
+    <h2>{id} - {recipe}</h2>
+    </div>
+    <div className="container">
+    <p>{ showMore ? description : description.substring(0, 200) + "..."}
+    <button onClick={()=>setShowMore(!showMore)} className="btn-one">
+      {showMore ? " Show less" : " Show more"}
+      </button>
+      </p>
+    </div>
+    <div className="btn">
+      <button onClick={previousCourse}>BACK</button>
+      <button onClick={nextCourse}>FORWARD</button>
+    </div>
+    </div>  
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        id: 1,
+        recipe: "Gingerbread Cookies",
+        image: "https://images.unsplash.com/photo-1485628926614-dbb863e7e1fa?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGdpbmdlciUyMGJyZWFkJTIwY29va2llc3xlbnwwfHwwfHx8MA%3D%3D",
+        description: "Beat the butter, then beat in the sugar. Mix in the egg and molasses. Sift the dry ingredients together. Gradually add the dry ingredients to the wet ingredients. Mix in the water. Pour the batter into the prepared baking pan. Bake until a toothpick comes out clean.",
+        showMore: false,
+    },
+
+    {
+        id: 2,
+        recipe: "Roast Turkey",
+        image: "https://images.unsplash.com/photo-1574672280600-4accfa5b6f98?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cm9hc3RlZCUyMHR1cmtleXxlbnwwfHwwfHx8MA%3D%3D",
+        description: "Remove the turkey from the fridge about 1 hour before roasting, to let it come to room temperature. Calculate the cooking time at 35 minutes per kg (so 2 hours 55 minutes for a 5kg turkey). Put the bird in a large roasting tin and set aside. In a pestle and mortar or spice grinder, finely grind the salt, fennel seeds and peppercorns; set aside. In a bowl, mash together the butter, citrus zests and garlic; set aside. Preheat the oven to 180˚C, gas mark 4. Carefully push your fingers between the skin and breast meat of the turkey to create 2 pockets, then push in the flavoured butter; smooth it down with your fingers. Sprinkle the fennel salt over the skin of the bird. Put the citrus halves and herbs into the cavity. Put the carrot, celery and onion in the tray; pour in the wine. Cover the tin in a double layer of thick foil, making sure it is well sealed. Roast for the calculated cooking time, minus 20 minutes. After this time, turn the oven up to 200˚C, gas mark 6, remove the foil from the turkey, drizzle the skin with a little olive oil and finish cooking, uncovered.",
+        showMore: false,
+    },
+
+    {
+        id: 3,
+        recipe: "Mushroom & chestnut Wellington",
+        image: "https://images.unsplash.com/photo-1581986675622-41eccf27129d?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWVhdCUyMHBpZXxlbnwwfHwwfHx8MA%3D%3D",
+        description: "Bring a large pan of water to the boil, add the spinach and blanch for 1 minute. Drain, refresh under cold water, then squeeze out as much liquid as possible. Roughly chop and set aside. Heat the butter and oil in a large saucepan and gently cook the leeks for 5 minutes, until softened and caramelised. Add the thyme and garlic and fry for 1 minute, then add the mushrooms and fry for 10-12 minutes more, until softened and any liquid has evaporated. Remove from the heat, stir in the chestnuts, tip into a large bowl and leave to cool. Stir the goat’s cheese and spinach through the cooled mushroom mixture. Lay a large sheet of baking parchment on the work surface and tip the mixture into the middle. Mould into a rough log shape, then roll up tightly in the parchment to make a 26cm-long cylinder. Chill until firm. Preheat the oven to 200°C, gas mark 6 and line a baking sheet with parchment. Scatter the work surface with flour and roll the pastry into a 34x28cm rectangle. Lay the cylinder of filling on top and unwrap. Whisk together the egg yolk and milk, then brush along the edges of the pastry. Carefully roll the filling in the pastry, finishing with the join on the underside.",
+        showMore: false,
+    },
+
+    {
+        id: 4,
+        recipe: "Buche de Noël",
+        image: "https://images.unsplash.com/photo-1607662255484-562ef2863903?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YnVjaGUlMjBkZSUyMG5vZWx8ZW58MHx8MHx8fDA%3D",
+        description: "Make yourself a treat and order your Christmass log at your favourite baker. Nothing better than a little bit of rest among all the Cristmass routine!",
+        showMore: false,
+    }
+]
